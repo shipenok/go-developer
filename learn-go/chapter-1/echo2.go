ab27e28280eaddf6704af657d506986d9a1ae82a
shipenok <andrei.shipenok@math.msu.ru>
2025-03-30 06:01:53 +0700

    first commit

---
+++ learn-go/chapter-1/echo2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+
+	for idx, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = ","
+		fmt.Println(idx, arg)
+	}
+	fmt.Println(s)
+}
